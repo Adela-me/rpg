e14df7fddfbee804d4eb416b107346b71cbafce5
Adela-me <adelame@maestralsolutions.com>
2022-09-08 01:58:53 +0200

    Adding Character DTOs

---
+++ rpg/Dtos/Character/CreateCharacterDto.cs
@@ -0,0 +1,14 @@
+﻿using rpg.Models;
+
+namespace rpg.Dtos.Character
+{
+    public class CreateCharacterDto
+    {
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass RpgClass { get; set; } = RpgClass.Knight;
+    }
+}
