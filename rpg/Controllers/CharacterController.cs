ee270ad39cc3d0ebfcb74597dff4c1873d38a0f9
Adela-me <adelame@maestralsolutions.com>
2022-09-07 16:55:12 +0200

    Adding Character controller

---
+++ rpg/Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rpg.Models;
+
+namespace rpg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
