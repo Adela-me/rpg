1e3ee0fbfb0ce86f238b4a7e21a634a6587e4d33
Adela-me <adelame@maestralsolutions.com>
2022-09-08 13:45:03 +0200

    User registration

---
+++ rpg/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rpg.Data;
+using rpg.Dtos.User;
+using rpg.Models;
+
+namespace rpg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            this.authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await authRepository.Register(
+                new User { Username = request.Username }, request.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return response;
+        }
+    }
+}
