37576e627c5cb485b3aaf67ff114daf631e039be
Adela-me <adelame@maestralsolutions.com>
2022-09-09 11:59:56 +0200

    Implementing Character-Weapon relationship

--- rpg/Data/DataContext.cs
+++ rpg/Data/DataContext.cs
@@ -12,6 +12,7 @@ namespace rpg.Data
 
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
 
     }
 }
